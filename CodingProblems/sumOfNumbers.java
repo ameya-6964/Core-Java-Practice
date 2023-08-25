a3f5bb35f9160978f96b901a03ba998de9484573
ameya-6964 <belvalkar7@gmail.com>
2023-08-25 15:03:04 +0530

    Coding Day 10 : Added sum Of Digits Code

---
+++ CodingProblems/sumOfNumbers.java
@@ -0,0 +1,32 @@
+/*
+        Day 10 coding Statement: Write a program to find Sum of digits of a number
+        Description
+        Get a number from user and then find the sum of the digits in the given number.
+        E.g.  let the number = 341
+        Sum of digits is 3+4+1= 8
+
+        Input
+        4521
+        Output
+        12
+*/
+
+
+package CodingProblems;
+import java.util.Scanner;
+public class sumOfNumbers {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int number = sc.nextInt();
+        System.out.println(digitAddition(number));
+    }
+    static int digitAddition(int n){
+        int sum = 0;
+        while(n!=0){
+            int lastNumber = n % 10;
+            sum += lastNumber;
+            n /= 10;
+        }
+        return sum;
+    }
+}
