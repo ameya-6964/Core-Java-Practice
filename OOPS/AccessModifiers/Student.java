95830bfb5a587cfa39cbf42d8680cc39686ae644
ameya-6964 <belvalkar7@gmail.com>
2023-09-15 19:04:07 +0530

    Added Acces Modifier Code

---
+++ OOPS/AccessModifiers/Student.java
@@ -0,0 +1,15 @@
+package OOPS.AccessModifiers;
+
+public class Student {
+    String name; //It Has Default access modifier So It Will Be Available In same Package
+    public int rollNo; // It Has Public Access Modifier It Is Available In All class And All Packages
+
+    protected String Address; // It has Protected Access Modifier Which Means
+
+}
+
+//PRIVATE :- Only Accessible in Same Class // Read And Write Only in Same Class // Accessible With getters And Setters
+
+//Default :- Accessible Within Same Class and Same Package
+
+// Public :- Accessible To All Class And All Packages;
---
+++ OOPS/AccessModifiers/StudentUse.java
@@ -0,0 +1,11 @@
+package OOPS.AccessModifiers;
+public class StudentUse {
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        Student s2 = new Student();
+        s1.name = "Ameya From ACCESS MODIFIER";
+        s1.rollNo = 1;
+        s2.name = "Kunal From ACCESS MODIFIER";
+        s2.rollNo = 3;
+    }
+}
