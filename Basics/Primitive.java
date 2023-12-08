53931289ffb11e169dca73c49b8a8b60e8073f0e
ameya-6964 <belvalkar7@gmail.com>
2023-12-08 21:04:55 +0530

    Added 8 Primitves

---
+++ Basics/Primitive.java
@@ -0,0 +1,38 @@
+package Basics;
+
+public class Primitive {
+    public static void main(String[] args) {
+        //Primitives 8 Types
+        //! byte
+        byte a = 100;
+        System.out.println(a);
+
+        //! short
+        short b = 70;
+        System.out.println(b);
+
+        //! int
+        int c = 10;
+        System.out.println(c);
+
+        //! long
+        long d = 10000000;
+        System.out.println(d);
+
+        //! float
+        float e = 3.14f;
+        System.out.println(e);
+
+        //! double
+        double f = 2.131313131d;
+        System.out.println(f);
+
+        //! char
+        char g = '2';
+        System.out.println(g);
+
+        //! boolean
+        boolean flag = false;
+        System.out.println(flag);
+    }
+}
