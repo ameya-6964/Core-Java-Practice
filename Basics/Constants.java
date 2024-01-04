b2fd733d8893348bf0b52a880710701b6f946516
ameya-6964 <belvalkar7@gmail.com>
2024-01-05 01:29:53 +0530

    Added final Variable Example

---
+++ Basics/Constants.java
@@ -0,0 +1,9 @@
+package Basics;
+
+public class Constants {
+    public static void main(String[] args) {
+        final float PIE = 3.14f;
+//        PIE = 12.3f; ERROR :- Cannot assign a value to final variable 'PIE'
+        System.out.println(PIE); //3.14
+    }
+}
