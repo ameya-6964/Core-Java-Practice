4bb40e9dff14e418296f5275771bb59ee0af151d
ameya-6964 <belvalkar7@gmail.com>
2024-02-09 20:23:04 +0530

    Completed Two Basic Tasks

---
+++ Basics/Main.java
@@ -0,0 +1,13 @@
+/*
+   Task 1 : Create a Class Main To Output "Good Morning" Abd Check The Output
+   Task 2 : Create New Variable As Name And Print Name In Console
+ */
+package Basics;
+
+    public class Main {
+        public static void main(String[] args) {
+            System.out.println("Good Morning");
+            String name = "Ameya";
+            System.out.println(name);
+        }
+    }
