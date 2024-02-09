e978af344acf83c17cd4202f7b1abd47c9567acd
ameya-6964 <belvalkar7@gmail.com>
2024-02-09 21:06:22 +0530

    Added Patterns Challenge

---
+++ Challenges/Challenge1.java
@@ -0,0 +1,43 @@
+package Challenges;
+/*
+Solve Following Patterns
+1)
+
+*
+* *
+* * *
+* * * *
+* * * * *
+
+2)
+* * * * *
+* * * *
+* * *
+* *
+*
+
+ */
+public class Challenge1 {
+    static void printRegularStar(int number){
+        for (int i = 0;i<number;i++){
+            for (int j=0;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void printReverseStar(int number) {
+        for (int i = number;i>0;i--){
+            for (int j=0;j<i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        printRegularStar(5);
+        printReverseStar(5);
+    }
+}
